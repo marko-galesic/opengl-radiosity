aed88d74143c405ca696b60e24a5a99bad9ec147
Tavor <mf.galesic@gmail.com>
2014-01-10 18:07:29 -0500

    Put in code to import triangles as patches.

--- vector.h
+++ vector.h
@@ -23,6 +23,9 @@ namespace Vectormath
 		point(float x, float y, float z);
 	};
 	Vector * crossproduct(Vector*, Vector*);
+	Vector * getVector(point * a, point * b);
 	float dotproduct(Vector a, Vector b);
 	Vector * add(point * a, Vector * b);
+	void normalize(Vector * a);
+	float length(Vector * a);
 }
